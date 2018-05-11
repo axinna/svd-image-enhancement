5adf9b1ab28d4748c8e09185d627ece4a58c8d3c
axin <527362824@qq.com>
2018-05-11 21:51:35 +0800

    1
    1

--- Matlab/testByAxin_dwtsvd_gray.m
+++ Matlab/testByAxin_dwtsvd_gray.m
@@ -5,12 +5,17 @@ clc;
 %%
 originalImage=imread('C:\Users\Administrator\Documents\GitHub\svd-image-enhancement\Aerial image\11.jpg');
 originalImage=rgb2gray(originalImage);                                      %变灰度图
-figure;imshow(originalImage);title('原始图像');
+figure;imshow(originalImage);title('');
 figure;
-%originalImage=im2double(originalImage);            %把图像unit8转换成double精度类型
+% originalImage=im2double(originalImage);            %把图像unit8转换成double精度类型
 
 %%
-[LL   HL    LH    HH] = dwt2(originalImage, 'haar'); 
+[LL   HL    LH    HH] = dwt2(originalImage, 'haar'); %奇怪！amazing！
+%%%%%%%%%%%%%%%%%%%%%LL在部分图片直接出现了去雾效果
+figure;imshow(LL,[])
+idwt2(LL, HL, LH, HH, 'haar');
+figure;imshow(ans,[])
+%%%%%%%%%%%%%%%%%%%%%%%
 [U    S     V       ] = svd(LL, 'econ');
 
 gheImage=GHE_gray(originalImage);
@@ -26,7 +31,7 @@ newLL=U*newS*V';
 newImage = idwt2(newLL, HL, LH, HH, 'haar');
 %newImage=im2uint8(newImage);
 newImage=round(newImage);
-imshow(newImage,[]);title('dwtsvd');
+imshow(newImage,[]);title('');
 figure;imhist(newImage/256);
 
 % finalImage=im2uint8(finalImage);  %把矩阵double转换成uint8类型
@@ -70,5 +75,6 @@ figure;imshow(newLL,[]);title('newLL')
 figure;imshow(newImage,[]);title('newImage')
 figure;imshow(newImage,[]);title('ReplaceImageAll')
 
-figure;imshow([LL HL;LH HH],[]);title('DWT')
-
+% figure;imshow([LL HL;LH HH],[]);title('DWT')
+%imrite(uint8(a),'tested.jpg','jpg');
+figure;imshow(uint8(newImage));
---
+++ Matlab/testByAxin_histgramOfNewImage.m
@@ -0,0 +1,29 @@
+% ª“∂»Õº
+clear all;
+close all;
+clc;
+%%
+originalImage=imread('C:\Users\Administrator\Documents\GitHub\svd-image-enhancement\Aerial image\111.png');
+originalImage=rgb2gray(originalImage);                                      %±‰ª“∂»Õº
+figure;imshow(originalImage);title('');
+figure;imhist(originalImage);title('');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
